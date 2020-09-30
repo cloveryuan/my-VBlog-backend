16b70e38bf569bde6684498c2f839132306bde3b
clover <750363796@qq.com>
2020-09-30 17:00:35 +0800

    第一次提交

---
+++ config/database.js
@@ -0,0 +1,33 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'localhost'),// strapi部署的服务器与MySQL所在的服务器是同一个服务器，主机就直接写localhost
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'Myblog'),
+        username: env('DATABASE_USERNAME', 'Myblog'),
+        password: env('DATABASE_PASSWORD', 'ma1201615624fzxj'),
+      },
+      options: {},
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6adecc5d-03a6-4d61-9752-f997792d4555'
+};
